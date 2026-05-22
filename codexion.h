7825b676ba4e8388b80ec004d00c3d70454a0fc6
Ilyes Naciri <inaciri@k2r2p6.42mulhouse.fr>
2026-05-22 16:50:35 +0200

    add: simlation struct

--- codexion.h
+++ codexion.h
@@ -6,7 +6,7 @@
 /*   By: inaciri <inaciri@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 14:30:49 by inaciri           #+#    #+#             */
-/*   Updated: 2026/05/22 16:23:15 by inaciri          ###   ########.fr       */
+/*   Updated: 2026/05/22 16:46:57 by inaciri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,16 @@ typedef struct P_arg
 	int		dongle_coooldown;
 	char	*scheduler;
 }			t_arg;
+
+typedef struct S_sim
+{
+	int				stop_flag;
+	pthread_mutex_t	m_flag;
+	pthread_mutex_t	m_print;
+	struct timeval	start;
+	struct P_arg	*param;
+}					t_sim;
+
 
 int	ft_parsing(char *data, char **argv);
 int	error_return(int z, char *arg_name);
